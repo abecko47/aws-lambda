e3c422d50ed8a3ddc0f85715ddea3a522952057a
Anatoliy <anatoliy.kotsur@seasy.at>
2023-11-22 05:39:54 +0100

    lint fix

--- aws-lambda/app.ts
+++ aws-lambda/app.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import {isValidToken} from "./middleware/auth";
-import {isRepeaterObject, Repeater} from "./util/dto";
+import { isValidToken } from './middleware/auth';
+import { isRepeaterObject, Repeater } from './util/dto';
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -12,18 +12,18 @@ import {isRepeaterObject, Repeater} from "./util/dto";
  */
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    if (!(await isValidToken(event.headers["Authorization"]))) {
+    if (!(await isValidToken(event.headers['Authorization']))) {
         return {
             statusCode: 401,
-            body: "Unauthorized",
+            body: 'Unauthorized',
         };
     }
 
     if (!event.body) {
         return {
             statusCode: 400,
-            body: "Bad request",
-        };;
+            body: 'Bad request',
+        };
     }
 
     const repeater: Repeater = JSON.parse(event.body);
@@ -31,28 +31,28 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
     if (!isRepeaterObject(repeater)) {
         return {
             statusCode: 400,
-            body: "Bad request",
+            body: 'Bad request',
         };
     }
 
-    if (repeater.action === "repeat") {
-        console.log({repeater});
+    if (repeater.action === 'repeat') {
+        console.log({ repeater });
         return {
             statusCode: 200,
             body: JSON.stringify(repeater),
         };
     }
 
-    if (repeater.action === "log") {
-        console.log({repeater});
+    if (repeater.action === 'log') {
+        console.log({ repeater });
         return {
             statusCode: 200,
-            body: "",
+            body: '',
         };
     }
 
     return {
         statusCode: 400,
-        body: "Bad request",
+        body: 'Bad request',
     };
 };
