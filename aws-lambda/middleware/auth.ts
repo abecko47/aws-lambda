f1ebf9d9f7818712c68648b369c8579e90cb4fe6
Anatoliy <anatoliy.kotsur@seasy.at>
2023-11-22 07:27:56 +0100

    prettier

--- aws-lambda/middleware/auth.ts
+++ aws-lambda/middleware/auth.ts
@@ -1,6 +1,6 @@
-import jwt, {Algorithm} from "jsonwebtoken";
-import JwksRsa from "jwks-rsa";
-import {jwtUrl} from "../util/const";
+import jwt, { Algorithm } from 'jsonwebtoken';
+import JwksRsa from 'jwks-rsa';
+import { jwtUrl } from '../util/const';
 
 export const isValidToken = async (token: string | undefined): Promise<boolean> => {
     if (!token) {
@@ -15,18 +15,20 @@ export const isValidToken = async (token: string | undefined): Promise<boolean> 
 
     const algorithm: Algorithm = decodedToken.header.alg as Algorithm;
 
-    const publicKey = (await JwksRsa({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: jwtUrl,
-    }).getSigningKey(decodedToken.header.kid)).getPublicKey();
+    const publicKey = (
+        await JwksRsa({
+            cache: true,
+            rateLimit: true,
+            jwksRequestsPerMinute: 5,
+            jwksUri: jwtUrl,
+        }).getSigningKey(decodedToken.header.kid)
+    ).getPublicKey();
 
     try {
-        jwt.verify(token, publicKey,{algorithms: [algorithm]});
+        jwt.verify(token, publicKey, { algorithms: [algorithm] });
     } catch {
-        return false
+        return false;
     }
 
     return true;
-}
+};
